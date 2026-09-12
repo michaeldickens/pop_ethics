c7635fbe261199d40ffaa48238b7e4f332d15da0
Michael Dickens <mdickens93@gmail.com>
2026-09-12 13:58:48 -0700

    update prep_logs

--- prep_logs.sh
+++ prep_logs.sh
@@ -2,4 +2,4 @@
 scp root@mdickens.me:/var/lib/pop-ethics/quiz-log.jsonl ./ && \
     python analyze_logs.py quiz-log.jsonl --html /tmp/report.html && \
     python analyze_logs.py quiz-log.jsonl --html /tmp/report-unfamiliar.html --familiarity no && \
-    python analyze_logs.py quiz-log.jsonl --html /tmp/report-fam.html --familiarity heard,explain
+    python analyze_logs.py quiz-log.jsonl --html /tmp/report-familiar.html --familiarity heard,explain
